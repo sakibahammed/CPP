1cefceb861f8282f82ffc3da7cbe4591f46dc8c6
sakibahammed <79939200+sakibahammed@users.noreply.github.com>
2022-06-14 14:50:41 +0600

    c++ initiated

---
+++ first.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    cout<<"Sakib Ahammed"<<endl;
+    cout <<"hello world"<<"hello"<<endl;
+    // cout
+    
+}
--- hello.cpp
+++ hello.cpp
@@ -1,8 +1,13 @@
 // Your First C++ Program
 
 #include <iostream>
+using namespace std;
 
 int main() {
-    std::cout << "Hello World!";
+    cout <<"Anisul islam \n Islam \n Rubel";
+    cout <<"HAziganj Chandpur";  
+    cout << 013564545454<<endl;
+    cout <<"NorthSouth university"<<endl;
+    cout << "ENter your id"<<endl;
     return 0;
-}
+}  
