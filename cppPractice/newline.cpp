1caf3173968cc127c979b92618f8b8940a22daeb
sakibahammed <79939200+sakibahammed@users.noreply.github.com>
2022-06-19 14:04:28 +0600

    variables.cpp

---
+++ cppPractice/newline.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std;
+int main(){
+
+    cout << "Boy I love bacon"<< endl;
+    cout<< " boy\n I\n love\n bacon"<<endl;
+    return 0;
+}
---
+++ cppPractice/variables.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+      int a = 4;
+      int b = 5; 
+      int c;
+      int sum=a+b;
+      cout<<sum<<endl; 
+     cin >> c;
+     cout<<c;
+      
+    return 0;
+}
